04cd99db24d3ec988d71a00df04cb43e4d8c3547
jorge <62824468+jotavev@users.noreply.github.com>
2023-09-26 22:39:28 -0300

    update: add user account and products in db_restart

--- appetieats/ext/commands.py
+++ appetieats/ext/commands.py
@@ -1,5 +1,8 @@
 from appetieats.ext.database import db
-from appetieats.models import Users, RestaurantsData
+from appetieats.models import (
+        Users, RestaurantsData, Categories, ProductImages, Products,
+        CustomersData
+)
 from werkzeug.security import generate_password_hash
 
 
@@ -30,8 +33,87 @@ def populate_users():
                 color="#4B8BBE",
                 url="python-pizza",
                 user_id=1
+            ),
+            Users(
+                id=2,
+                username="wanteat",
+                hash=generate_password_hash("wanteat"),
+                role="customer"
+            ),
+            CustomersData(
+                id=1,
+                first_name="want",
+                last_name="eat",
+                phone="555-hungry",
+                address="Craving St, 789",
+                zip_code="98765",
+                reference="near to rumbling belly",
+                user_id=2
+            ),
+            Categories(
+                id=1,
+                user_id=1,
+                category_name="Pizza"
+            ),
+            ProductImages(
+                id=1,
+                product_id=1,
+                image_path="product1.png",
+                image_data=open(
+                    "appetieats/static/assets/img/pizza.png", "rb"
+                    ).read()
+            ),
+            ProductImages(
+                id=2,
+                product_id=2,
+                image_path="product2.png",
+                image_data=open(
+                    "appetieats/static/assets/img/pizza.png", "rb"
+                    ).read()
+            ),
+            ProductImages(
+                id=3,
+                product_id=3,
+                image_path="product3.png",
+                image_data=open(
+                    "appetieats/static/assets/img/pizza.png", "rb"
+                    ).read()
+            ),
+            Products(
+                id=1,
+                name="Flask 'n' Cheese Pizza",
+                description="A classic pizza with tomato "
+                "sauce and melted cheese. A tribute to Flask, a lightweight "
+                "Python web framework.",
+                price=10.99,
+                barcode=1,
+                available="True",
+                category_id=1,
+                user_id=1
+            ),
+            Products(
+                id=2,
+                name="Python Pepperoni Powerhouse",
+                description="A pepperoni lover's dream with generous slices "
+                "of pepperoni. Pay homage to the mighty Python programming "
+                "language.",
+                price=14.99,
+                barcode=2,
+                available="True",
+                category_id=1,
+                user_id=1
+            ),
+            Products(
+                id=3,
+                name="Pythonic Pineapple Paradise",
+                description="A sweet and savory blend of pineapple and ham "
+                "with a Pythonic twist.",
+                price=13.99,
+                barcode=3,
+                available="True",
+                category_id=1,
+                user_id=1
             )
-            # TODO: add products
     ]
     db.session.bulk_save_objects(data)
     db.session.commit()
