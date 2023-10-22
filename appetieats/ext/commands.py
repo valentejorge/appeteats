c3e8d9dc42f4110264736dc1ba24fffa7e614292
jorge <62824468+jotavev@users.noreply.github.com>
2023-10-22 00:57:28 -0300

    update: new approach to to populate sample date with json

--- appetieats/ext/commands.py
+++ appetieats/ext/commands.py
@@ -53,118 +53,13 @@ def populate_database_from_json():
             product_image = ProductImages(
                 product_id=product_id,
                 image_path=image_path,
-                # TODO: change image path to a real path
                 image_data=open(
-                    "appetieats/static/assets/img/pizza.png", "rb"
+                    f"appetieats/static/sample_data/{image_path}", "rb"
                     ).read()
             )
             db.session.add(product_image)
 
         db.session.commit()
-
-
-def populate_users():
-    """populate db users with default accounts"""
-    data = [
-            Users(
-                id=1,
-                username="dev",
-                hash=generate_password_hash("dev"),
-                role="restaurant"
-            ),
-            RestaurantsData(
-                id=1,
-                name="Python Pizza",
-                address="123 Snake St, Pythonville, PY 31415",
-                phone="555-PY-314",
-                color="#4B8BBE",
-                url="python-pizza",
-                user_id=1
-            ),
-            Users(
-                id=2,
-                username="wanteat",
-                hash=generate_password_hash("wanteat"),
-                role="customer"
-            ),
-            CustomersData(
-                id=1,
-                first_name="want",
-                last_name="eat",
-                phone="555-hungry",
-                address="Craving St, 789",
-                zip_code="98765",
-                reference="near to rumbling belly",
-                user_id=2
-            ),
-            Categories(
-                id=1,
-                user_id=1,
-                category_name="Pizza"
-            ),
-            ProductImages(
-                id=1,
-                product_id=1,
-                image_path="product1.png",
-                image_data=open(
-                    "appetieats/static/assets/img/pizza.png", "rb"
-                    ).read()
-            ),
-            ProductImages(
-                id=2,
-                product_id=2,
-                image_path="product2.png",
-                image_data=open(
-                    "appetieats/static/assets/img/pizza.png", "rb"
-                    ).read()
-            ),
-            ProductImages(
-                id=3,
-                product_id=3,
-                image_path="product3.png",
-                image_data=open(
-                    "appetieats/static/assets/img/pizza.png", "rb"
-                    ).read()
-            ),
-            Products(
-                id=1,
-                name="Flask 'n' Cheese Pizza",
-                description="A classic pizza with tomato "
-                "sauce and melted cheese. A tribute to Flask, a lightweight "
-                "Python web framework.",
-                price=10.99,
-                barcode=1,
-                available="True",
-                category_id=1,
-                user_id=1
-            ),
-            Products(
-                id=2,
-                name="Python Pepperoni Powerhouse",
-                description="A pepperoni lover's dream with generous slices "
-                "of pepperoni. Pay homage to the mighty Python programming "
-                "language.",
-                price=14.99,
-                barcode=2,
-                available="True",
-                category_id=1,
-                user_id=1
-            ),
-            Products(
-                id=3,
-                name="Pythonic Pineapple Paradise",
-                description="A sweet and savory blend of pineapple and ham "
-                "with a Pythonic twist.",
-                price=13.99,
-                barcode=3,
-                available="True",
-                category_id=1,
-                user_id=1
-            )
-    ]
-    db.session.bulk_save_objects(data)
-    db.session.commit()
-    return Users.query.all()
 
 
 def hello_commands():
@@ -177,14 +72,13 @@ def restart_db():
     drop_db()
     create_db()
     populate_database_from_json()
-    # populate_users()
 
 
 def init_app(app):
     """add multiple commands in a bulk"""
     for command in [create_db,
                     drop_db,
-                    populate_users,
+                    populate_database_from_json,
                     hello_commands,
                     restart_db]:
         app.cli.add_command(app.cli.command()(command))
