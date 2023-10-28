ac00ffcd6bbcb207fdc13def3175aed642e1fd8a
jorge <62824468+jotavev@users.noreply.github.com>
2023-10-28 15:19:07 -0300

    update: adjusting to comply with PEP 8

--- appetieats/ext/helpers/cache_images.py
+++ appetieats/ext/helpers/cache_images.py
@@ -1,10 +1,12 @@
+"""Module to cache images from the database"""
 import os
 from appetieats.models import ProductImages
 
 
 def get_image(image_name, image_id):
-    CACHE_DIR = "appetieats/static/cache"
-    cache_file_path = os.path.join(CACHE_DIR, image_name)
+    """put a image from db into a cache dir"""
+    cache_dir_path = "appetieats/static/cache"
+    cache_file_path = os.path.join(cache_dir_path, image_name)
 
     if not os.path.exists(cache_file_path):
         image_data = ProductImages.query.get(image_id).image_data
