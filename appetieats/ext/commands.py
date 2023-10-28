92fbeb5bc355a6a9ac37e635d067026cb6ad1a0a
jorge <62824468+jotavev@users.noreply.github.com>
2023-10-28 15:13:18 -0300

    update: update db function more smart

--- appetieats/ext/commands.py
+++ appetieats/ext/commands.py
@@ -1,11 +1,12 @@
-import json
+"""Module providing a commands to flask"""
 import os
+import json
+from werkzeug.security import generate_password_hash
 from appetieats.ext.database import db
 from appetieats.models import (
         Users, RestaurantsData, Categories, ProductImages, Products,
         CustomersData
 )
-from werkzeug.security import generate_password_hash
 
 
 def create_db():
@@ -19,45 +20,49 @@ def drop_db():
 
 
 def populate_database_from_json():
-    json_file = 'appetieats/ext/helpers/sample_data.json'
-    print(os.path.abspath(json_file))
+    """populate db from a json file"""
+    json_path = "appetieats/ext/helpers/sample_data.json"
+    print(os.path.abspath(json_path))
 
-    with open(json_file, 'r') as file:
-        data = json.load(file)
+    with open(json_path, "r", encoding="utf-8") as file:
+        json_file = json.load(file)
 
-        for user_data in data['users']:
-            user_data['hash'] = generate_password_hash(user_data['hash'])
-            user = Users(**user_data)
-            db.session.add(user)
+        for key in json_file:
+            for data in json_file[f"{key}"]:
+                match key:
+                    case "users":
+                        data["hash"] = generate_password_hash(data["hash"])
+                        user = Users(**data)
+                        db.session.add(user)
 
-        for restaurant_data in data['restaurants']:
-            restaurant = RestaurantsData(**restaurant_data)
-            db.session.add(restaurant)
+                    case "restaurants":
+                        restaurant = RestaurantsData(**data)
+                        db.session.add(restaurant)
 
-        for categories_data in data['categories']:
-            category = Categories(**categories_data)
-            db.session.add(category)
+                    case "categories":
+                        category = Categories(**data)
+                        db.session.add(category)
 
-        for customer_data in data['customers']:
-            customer = CustomersData(**customer_data)
-            db.session.add(customer)
+                    case "customers":
+                        customer = CustomersData(**data)
+                        db.session.add(customer)
 
-        for product_data in data['products']:
-            product = Products(**product_data)
-            db.session.add(product)
+                    case "products":
+                        product = Products(**data)
+                        db.session.add(product)
 
-        for image_data in data['product_images']:
-            image_path = image_data['image_path']
-            product_id = image_data['product_id']
+                    case "product_images":
+                        image_path = data["image_path"]
+                        product_id = data["product_id"]
 
-            product_image = ProductImages(
-                product_id=product_id,
-                image_path=image_path,
-                image_data=open(
-                    f"appetieats/static/sample_data/{image_path}", "rb"
-                    ).read()
-            )
-            db.session.add(product_image)
+                        product_image = ProductImages(
+                            product_id=product_id,
+                            image_path=image_path,
+                            image_data=open(
+                                f"appetieats/static/sample_data/{image_path}", "rb"
+                                ).read()
+                        )
+                        db.session.add(product_image)
 
         db.session.commit()
 
