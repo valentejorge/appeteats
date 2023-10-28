e4967386767ed768e6eede8b171d83fdd9597476
jorge <62824468+jotavev@users.noreply.github.com>
2023-10-28 15:19:54 -0300

    update: adjusting to comply with PEP 8

--- appetieats/ext/helpers/validate_inputs.py
+++ appetieats/ext/helpers/validate_inputs.py
@@ -1,93 +1,96 @@
-from flask import abort, session, flash, redirect
-from appetieats.models import Categories, Users, RestaurantsData
+"""Module providing a tools for validate user inputs"""
+from flask import abort, session
 from werkzeug.security import check_password_hash
+from appetieats.models import Categories, Users, RestaurantsData
 
 
 def validate_product_data(product_data):
+    """validate data of product"""
     if not product_data["name"]:
         return abort(403, "You must provide a product name")
 
-    elif not product_data["price"]:
+    if not product_data["price"]:
         return abort(403, "You must provide a price")
 
-    elif not str(product_data["price"]).replace('.', '', 1).isdigit():
+    if not str(product_data["price"]).replace('.', '', 1).isdigit():
         return abort(403, "You must provide a valid price")
 
-    elif not product_data["description"]:
+    if not product_data["description"]:
         return abort(403, "You must provide a description")
 
-    elif not product_data["barcode"]:
+    if not product_data["barcode"]:
         return abort(403, "You must provide a barcode")
 
-    elif not product_data["category"]:
+    if not product_data["category"]:
         return abort(403, "You must provide a category")
 
-    elif Categories.query.filter_by(
+    if Categories.query.filter_by(
             id=product_data["category"],
             user_id=session.get("user_id")).first() is None:
         return abort(403, "You must provide a valid category")
-    else:
-        return
+
+    return None
 
 
 def validate_product_image(product_image):
+    """validate image of product"""
     if not product_image:
         return abort(403, "You must provide a product image")
-    else:
-        return
+    return None
 
 
 def validate_user_register_data(username, password, confirm):
+    """validate data of user"""
     if not username:
         return abort(403, "You must provide a username")
 
-    elif not password:
+    if not password:
         return abort(403, "You must provide a password")
 
-    elif not confirm:
+    if not confirm:
         return abort(403, "You must confirm your password")
 
-    elif password != confirm:
+    if password != confirm:
         return abort(403, "The passwords dont match")
 
-    elif Users.query.filter_by(username=username).first() is not None:
+    if Users.query.filter_by(username=username).first() is not None:
         return abort(403, "The username already exists")
 
-    else:
-        return
+    return None
 
 
 def validate_credentials(username, password, account_type):
+    """validate credentials of user"""
     user = Users.query.filter_by(username=username).first()
 
     if not username or not password:
         return abort(403, "Must provide username/password")
 
-    elif not user:
+    if not user:
         return abort(403, "Invalid user")
 
-    elif user.role != account_type:
+    if user.role != account_type:
         if account_type == "restaurant":
             return abort(
                     403,
                     "Your account is of customer type. "
                     "Please log in with a restaurant account"
             )
-        elif account_type == "customer":
+        if account_type == "customer":
             return abort(
                     403,
                     "Your account is of restaurant type. "
                     "Please log in with a customer account"
             )
 
-    elif not check_password_hash(user.hash, str(password)):
+    if not check_password_hash(user.hash, str(password)):
         return abort(403, "Wrong password")
 
-    else:
-        return
+    return None
 
 
 def validate_user_data(user_data):
+    """validate all inputs from user form"""
     for field, value in user_data.items():
         if not value:
             return abort(
@@ -95,10 +98,12 @@ def validate_user_data(user_data):
                     "You are attempting to register a restaurant account in "
                     "the customer route or vice versa."
             )
-    return
+
+    return None
 
 
 def validate_cart(products, cart_dict, restaurant_id):
+    """validate products of cart"""
     products_ids = {product.id for product in products}
     if len(cart_dict) == 0:
         return False
@@ -106,13 +111,14 @@ def validate_cart(products, cart_dict, restaurant_id):
         if item["id"] not in products_ids:
             return False
 
-        elif item["restaurant_id"] != restaurant_id:
+        if item["restaurant_id"] != restaurant_id:
             return False
 
     return True
 
 
 def validate_user_account():
+    """validate user account"""
     if not session.get("user_id"):
         return False
     if not session.get("role") == "customer":
@@ -121,8 +127,10 @@ def validate_user_account():
 
 
 def validate_user_url(user_url):
+    """validate user url"""
     url = RestaurantsData.query.filter_by(url=user_url).first()
 
     if url:
-        return abort(403, "the url exists, try another")
-    return
+        return abort(403, "the url dont exists, try another")
+
+    return None
