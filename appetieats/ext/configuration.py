ca6bcaabb5339097e97678b9f5b2185fa90ac03d
jorge <62824468+jotavev@users.noreply.github.com>
2023-10-28 15:15:55 -0300

    update: adjusting to comply with PEP 8

--- appetieats/ext/configuration.py
+++ appetieats/ext/configuration.py
@@ -1,12 +1,15 @@
+"""Module to configure the application"""
 from importlib import import_module
 from dynaconf import FlaskDynaconf
 
 
 def load_extensions(app):
+    """load extensions from toml file"""
     for extension in app.config.get("EXTENSIONS"):
         mod = import_module(extension)
         mod.init_app(app)
 
 
 def init_app(app):
+    """init dynaconf"""
     FlaskDynaconf(app)
