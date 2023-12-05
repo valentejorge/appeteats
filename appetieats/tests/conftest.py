f5448434bf384d9a5f00fe52931a845170dfc1ba
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-12-04 23:42:27 -0300

    update: tests

---
+++ appetieats/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from appetieats.app import create_app
+from appetieats.ext.commands import restart_testing_db
+from appetieats.ext.database import db
+
+
+@pytest.fixture(scope="session")
+def app():
+    app = create_app(FORCE_ENV_FOR_DYNACONF="testing")
+
+    with app.app_context():
+        restart_testing_db()
+
+        yield app
+        db.drop_all()
+
+
+@pytest.fixture(scope="session")
+def client(app):
+    return app.test_client()
