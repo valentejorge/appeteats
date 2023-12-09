59d3923b79a8a1003eeaa24003008c279a921f1b
jorge_thinkpad <62824468+jotavev@users.noreply.github.com>
2023-12-08 22:47:53 -0300

    update: add paths for tests env

--- appetieats/ext/helpers/cache_images.py
+++ appetieats/ext/helpers/cache_images.py
@@ -1,15 +1,20 @@
 """Module to cache images from the database"""
 import os
 from appetieats.models import ProductImages
+from flask import current_app
 
 
 def get_image(image_name, image_id):
     """put a image from db into a cache dir"""
-    cache_dir_path = "appetieats/static/cache"
+    # cache_dir_path = "appetieats/static/cache"
+    cache_dir_path = current_app.config.get("CACHE_PATH")
+
+    print(cache_dir_path)
     cache_file_path = os.path.join(cache_dir_path, image_name)
 
     if not os.path.exists(cache_file_path):
-        image_data = ProductImages.query.get(image_id).image_data
+        image_data = ProductImages.query.filter(
+                ProductImages.id == image_id).first().image_data
 
         with open(cache_file_path, "wb") as file:
             file.write(image_data)
